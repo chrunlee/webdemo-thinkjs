ec2453095ccb5f5dee5716d1cfc045f40b1e3b45
chrunlee <chrunlee@foxmail.com>
2019-10-17 15:12:50 +0800

    debug repeat

--- src/controller/api.js
+++ src/controller/api.js
@@ -41,78 +41,82 @@ module.exports = class extends Base {
 
     async fileToOssAction(){
         //将本地资源图片，上传到OSS
-        let ak = this.config('site').qiniuak.value;
-        let sk = this.config('site').qiniusk.value;
-        let scope = this.config('site').qiniuscope.value;
-        let staticdomain = this.config('site').staticdomain.value;
-        //1.以数据库为基础进行处理。 
-        let baseFolder = path.join(think.ROOT_PATH,'www');
-        //一、order_goods ： picpath.
-        let goodList = await this.model('order_goods').field('id,picpath').select();
-        for(let index in goodList){
-            let good = goodList[index];
-            let picpath = good.picpath;
-            let goodId = good.id;
-            let realPath = path.join(baseFolder,picpath);
-            if(fs.existsSync(realPath) && think.isFile(realPath)){
-                let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
-                //更新数据库。
-                await this.model('order_goods').where({id : goodId}).update({picpath : staticdomain+'/'+filePath});
-            }else{
-                console.log(realPath+' ： 文件不存在。')
-            }
-        }
-
-        //二、user_article : postpath,content
-         let articleList = await this.model('user_article').field('id,postpath,content').select();
-         for(let i in articleList){
-            let article = articleList[i];
-            let articleId = article.id,
-                postpath = article.postpath,
-                content = article.content;
-            //(/upload/user/1537336644065-20.png)
-            let realPath = path.join(baseFolder,postpath);
-            if(fs.existsSync(realPath) && think.isFile(realPath)){
-                let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
-                //更新数据库。
-                await this.model('user_article').where({id : articleId}).update({postpath : staticdomain+'/'+filePath});
-            }
-            //查找content内容
-            let matchRs = content.match(/\((\/upload.*)\)/g);
-            if(matchRs){
-                for(let rsIndex in matchRs){
-                    let str = matchRs[rsIndex];
-                    let tempPath = str.replace('(','').replace(')','');
-                    let tempRealPath = path.join(baseFolder,tempPath);
-                    if(fs.existsSync(tempRealPath) && think.isFile(tempRealPath)){
-                        let tempFinalPath = await qiniuCloud.saveFile(ak,sk,scope,tempRealPath,'static_');
-                        //替换
-                        content = content.replace(str,'('+staticdomain+'/'+tempFinalPath+')');
-                    }else{
-                        console.log(tempRealPath+'： 文件不存在');
-                    }
+        try{
+            let ak = this.config('site').qiniuak.value;
+            let sk = this.config('site').qiniusk.value;
+            let scope = this.config('site').qiniuscope.value;
+            let staticdomain = this.config('site').staticdomain.value;
+            //1.以数据库为基础进行处理。 
+            let baseFolder = path.join(think.ROOT_PATH,'www');
+            //一、order_goods ： picpath.
+            let goodList = await this.model('order_goods').field('id,picpath').select();
+            for(let index in goodList){
+                let good = goodList[index];
+                let picpath = good.picpath;
+                let goodId = good.id;
+                let realPath = path.join(baseFolder,picpath);
+                if(fs.existsSync(realPath) && think.isFile(realPath)){
+                    let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
+                    //更新数据库。
+                    await this.model('order_goods').where({id : goodId}).update({picpath : staticdomain+'/'+filePath});
+                }else{
+                    console.log(realPath+' ： 文件不存在。')
                 }
-                await this.model('user_article').where({id : articleId}).update({content : content});
             }
-         }
-        //三、user_banner : bannerpath
-        let bannerList = await this.model('user_banner').field('id,bannerpath').select();
-        for(let index in bannerList){
-            let good = bannerList[index];
-            let picpath = good.bannerpath;
-            let goodId = good.id;
-            let realPath = path.join(baseFolder,picpath);
-            if(fs.existsSync(realPath) && think.isFile(realPath)){
-                let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
-                //更新数据库。
-                await this.model('user_banner').where({id : goodId}).update({bannerpath : staticdomain+'/'+filePath});
-            }else{
-                console.log(realPath+' ： 文件不存在。')
+
+            //二、user_article : postpath,content
+             let articleList = await this.model('user_article').field('id,postpath,content').select();
+             for(let i in articleList){
+                let article = articleList[i];
+                let articleId = article.id,
+                    postpath = article.postpath,
+                    content = article.content;
+                //(/upload/user/1537336644065-20.png)
+                let realPath = path.join(baseFolder,postpath);
+                if(fs.existsSync(realPath) && think.isFile(realPath)){
+                    let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
+                    //更新数据库。
+                    await this.model('user_article').where({id : articleId}).update({postpath : staticdomain+'/'+filePath});
+                }
+                //查找content内容
+                let matchRs = content.match(/\((\/upload.*)\)/g);
+                if(matchRs){
+                    for(let rsIndex in matchRs){
+                        let str = matchRs[rsIndex];
+                        let tempPath = str.replace('(','').replace(')','');
+                        let tempRealPath = path.join(baseFolder,tempPath);
+                        if(fs.existsSync(tempRealPath) && think.isFile(tempRealPath)){
+                            let tempFinalPath = await qiniuCloud.saveFile(ak,sk,scope,tempRealPath,'static_');
+                            //替换
+                            content = content.replace(str,'('+staticdomain+'/'+tempFinalPath+')');
+                        }else{
+                            console.log(tempRealPath+'： 文件不存在');
+                        }
+                    }
+                    await this.model('user_article').where({id : articleId}).update({content : content});
+                }
+             }
+            //三、user_banner : bannerpath
+            let bannerList = await this.model('user_banner').field('id,bannerpath').select();
+            for(let index in bannerList){
+                let good = bannerList[index];
+                let picpath = good.bannerpath;
+                let goodId = good.id;
+                let realPath = path.join(baseFolder,picpath);
+                if(fs.existsSync(realPath) && think.isFile(realPath)){
+                    let filePath = await qiniuCloud.saveFile(ak,sk,scope,realPath,'static_');
+                    //更新数据库。
+                    await this.model('user_banner').where({id : goodId}).update({bannerpath : staticdomain+'/'+filePath});
+                }else{
+                    console.log(realPath+' ： 文件不存在。')
+                }
             }
+            //四、user_attach : 用于自己的图床。--暂时保留
+
+            return this.json({msg : 'fuck over'});
+        }catch(e){
+            console.log(e);
+            return this.json({msg : 'fuck error'});
         }
-        //四、user_attach : 用于自己的图床。--暂时保留
-
-        return this.json({msg : 'fuck over'});
-
     }
 }
