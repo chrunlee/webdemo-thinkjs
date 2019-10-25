220de4b8c926aff68df509cbc3611e8a69ef0276
chrunlee <chrunlee@foxmail.com>
2019-10-25 13:21:14 +0800

    add login router

--- src/config/router.js
+++ src/config/router.js
@@ -2,6 +2,7 @@ module.exports = [
     [/^\/article\/(.*)$/,'/article/detail?id=:1'],
     [/^\/detail\/(.*)$/i,'/article/detail?id=:1'],
     [/^\/shop\/detail\/(.*)$/,'/shop/detail?id=:1'],
-    [/^\/shop\/deal\/(.*)$/,'/shop/deal?id=:1']
+    [/^\/shop\/deal\/(.*)$/,'/shop/deal?id=:1'],
+    [/^\/login$/,'/index/login']
     // [/^.*$/,'/error']
 ];
