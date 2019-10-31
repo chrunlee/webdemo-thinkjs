180f7565b4bbb4852cdc889e5ac131b96e74900f
chrunlee <chrunlee@foxmail.com>
2019-10-31 18:33:03 +0800

    add music of 163

--- src/controller/tool.js
+++ src/controller/tool.js
@@ -1,5 +1,8 @@
 //工具类相关的页面处理
 const Base = require('./base.js');
+const axios = require('axios');
+// const URL = require('url');
+const music163 = require('../util/music163');
 
 module.exports = class extends Base {
     //微信dat转码页面
@@ -9,6 +12,98 @@ module.exports = class extends Base {
         })
         return this.display('tool/wechatdat');
     }
+    //跳转到网易云音乐
+    async music163Action(){
+        this.assign('site',this.config('site'))
+        //检查code
+        let code = await this.session('netmusiccode');
+        let isFirst = await this.session('netmusicfirst')||1;
+        console.log(isFirst);
+        if( (code == null || code == undefined || code == '') && isFirst >= 3){
+            //无code
+            let codestr = this.post('code')||'';
+            //check code
+            let codeObj = await this.model('user_code').where({code : codestr.trim(),type : 'netmusic'}).find();
+            if(think.isEmpty(codeObj)){
+                this.assign('code',false);
+                this.assign('msg','当前校验码无效，请重新填写或联系站长!')
+                this.assign('success',false);
+                return this.display('tool/netmusic');
+            }
+            await this.session('netmusiccode',codeObj.code);
+            this.assign('codestr',codeObj.code);
+        }
+        if(isFirst < 3){
+            console.log('当前为试用状态'+isFirst)
+            this.assign('codestr','shiyong');
+            this.assign('success',true);
+            this.assign('msg','');
+        }else{
+            this.assign('codestr',code);
+        }
+        if(this.isPost){
+            let urlstr = this.post('url');
+            try{
+                if(urlstr){
+                    //分析url
+                    let type = -1;//0 单曲，1 歌单，2 MV
+                    let urlObj = new URL(urlstr);
+                    let id = urlObj.searchParams.get('id')||'';
+                    if(!id){
+                        this.assign({success : false,msg : 'url地址不符合规范，无法获取ID数据'})
+                    }else{
+                        if(urlObj.pathname.indexOf('song') > -1){
+                            type = 0;
+                        }else if(urlObj.pathname.indexOf('playlist') > -1){
+                            type = 1;
+                        }else if(urlObj.pathname.indexOf('video') > -1){
+                            type = 2;
+                        }
+                        if(type >= 0){
+                            //单首，直接获取数据和url
+                            let musicObj = new music163(this.config('site').netmusicapi.value,this.config('site').netmusicphone.value,this.config('site').netmusicpwd.value);
+                            let cookie = this.config('netmusiccookie');
+                            if(think.isEmpty(cookie)){
+                                cookie =await musicObj.login();
+                                this.config('netmusiccookie',cookie);
+                            }
+                            if(isFirst < 3 && type > 0){
+                                this.assign({success : false,msg : '试用下不支持歌单和视频'})
+                            }else{
+                                if(type == 0){
+                                    let detail = await musicObj.getSingle(cookie,id);
+                                    this.assign('list',[detail]);
+                                }else if(type == 1){
+                                    let list = await musicObj.getList(cookie,id);
+                                    this.assign(list);
+                                    this.assign('playlist',true);
+                                }else if(type == 2){
+                                    let rs = await musicObj.getMV(cookie,id);
+                                    this.assign('list',[rs]);
+                                }
+                                if(isFirst < 3){
+                                    isFirst +=1;
+                                    await this.session('netmusicfirst',isFirst);
+                                }
+                            }
+                        }else{
+                            this.assign({success : false,msg : '无法解析地址或地址不规范，目前只支持:单首/歌单/视频。'})
+                        }
+                    }
+                }else{
+                    this.assign({success : true,msg : 'url不能为空'})
+                }
+            }catch(e){
+                console.log(e);
+                this.assign({success : false,msg : '网易云API解析服务器无法链接!'})
+            }
+            this.assign('url',urlstr);
+            return this.display('tool/netmusic');
 
-
+        }else{
+            return this.display('tool/netmusic');    
+        }
+        
+    }
+    
 }
---
+++ src/util/music163.js
@@ -0,0 +1,136 @@
+//登录网易云音乐 获取数据
+let axios = require('axios');
+let path = require('path');
+let url = require('url');
+function format(  size, pointLength, units ){
+    var unit;
+    units = units || [ 'B', 'KB', 'M', 'G', 'TB' ];
+
+    while ( (unit = units.shift()) && size > 1024 ) {
+        size = size / 1024;
+    }
+    return (unit === 'B' ? size : size.toFixed( pointLength || 2 )) + unit;
+}
+class music {
+    constructor (api,phone,password){
+        this.api = api;
+        this.phone = phone;
+        this.password = password;
+        this.loginUrl = '/login/cellphone';
+
+        return this;
+    }
+    //登录获取cookie
+    login (){
+        return axios.get(`${this.api}/login/cellphone?phone=${this.phone}&password=${this.password}`).then(rs=>{
+            let cookie = rs.headers['set-cookie'];
+            return cookie[0]+cookie[1];
+        });
+    }
+    //根据ID获取歌曲的名字和基本信息
+    getMusicDetail (cookie/**cookie存在其他地方*/,ids/**数组或字符串逗号隔开*/){
+        ids = typeof ids == Array ? ids.join(',') : ids;
+        return axios.get(`${this.api}/song/detail?ids=${ids}`,{headers : {cookie : cookie}}).then(rs=>{
+            let data = rs.data;
+            return data.songs.map(item=>{
+                return {
+                    id : item.id,name : item.name
+                }
+            });   
+        });
+    }
+    //根据ID获取多个歌曲的url地址信息
+    getMusicUrl(cookie,ids){
+        ids = typeof ids == Array ? ids.join(',') : ids;
+        return axios.get(`${this.api}/song/url?id=${ids}`,{headers : {cookie : cookie}}).then(rs=>{
+            console.log(rs.data);
+            let data = rs.data;
+            return data.data;
+        });
+    }
+    //获取某个歌单的详情数据，内含多个歌曲URL和详细信息
+    getPlayListDetail (cookie,id){
+        return axios.get(`${this.api}/playlist/detail?id=${id}`,{headers : {cookie : cookie}})
+        .then(rs=>{
+            let data = rs.data;
+            //处理data获取ID，根据ID获取url，然后组合
+            return {
+                name : data.playlist.name,
+                list : data.playlist.trackIds
+            };
+        })
+    }
+
+    getMvDetail(cookie,id){
+        return axios.get(`${this.api}/video/detail?id=${id}`,{headers : {cookie : cookie}})
+        .then(rs=>{
+            return rs.data;
+        })
+    }
+    getMvUrl (cookie,id){
+        return axios.get(`${this.api}/video/url?id=${id}`,{headers : {cookie : cookie}})
+        .then(rs=>{
+            return rs.data;
+        })
+    }
+    //获得视频的数据
+    async getMV(cookie,id){
+        let detail = await this.getMvDetail(cookie,id);
+        var name = detail.data.title;
+        let urlrs = await this.getMvUrl(cookie,id);
+        let urls = urlrs.urls[0].url;
+        let size = urlrs.urls[0].size;
+        return {
+            id : id,
+            name : name+path.extname(url.parse(urls||'').pathname),
+            url : urls,
+            size : format(size),
+            success : urls == null ? false : true
+        }
+    }
+    //单曲
+    async getSingle (cookie,id){
+        let detail = await this.getMusicDetail(cookie,id);
+        let ids = detail[0].id;
+        let urldetail = await this.getMusicUrl(cookie,ids);
+        return {
+            id : detail[0].id,
+            name : detail[0].name+path.extname(url.parse(urldetail[0].url||'').pathname),
+            url : urldetail[0].url,
+            size : format(urldetail[0].size),
+            success : urldetail[0].url == null ? false : true
+        }
+    }
+    //歌单
+    async getList(cookie,id){
+        let playlist = await this.getPlayListDetail(cookie,id);
+        let ids = playlist.list.map(item=>item.id);
+        let detailList = await this.getMusicDetail(cookie,ids);
+        let urlList = await this.getMusicUrl(cookie,ids);
+        //组装数据
+        let detailMap = {};
+        detailList.forEach(item=>{
+            detailMap[item.id] = item;
+        })
+        let urlMap = {};
+        urlList.forEach(item=>{
+            urlMap[item.id] = item;
+        })
+        let rs = ids.map(tempId=>{
+            let de = detailMap[tempId];
+            let ur = urlMap[tempId];
+            return {
+                id : tempId,
+                name : de.name+path.extname(url.parse(ur.url||'').pathname),
+                url : ur.url,
+                size : format(ur.size),
+                success : ur.url == null  ? false : true
+            }
+        });
+        return {
+            name : playlist.name,
+            list : rs
+        };
+    }
+}
+module.exports = music;
